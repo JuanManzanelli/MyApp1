125c45c44f583d33d48f336bb2661959f6cbf8b9
Juan Manzanelli <manzanellijc@gmail.com>
2022-06-15 23:12:29 -0300

    Se agrega el Proyecto

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+
+  <header className='navbar'>
+    <button>My Sleep</button>
+    <p>Catalogo</p>
+    <p>Contacto</p>
+    <a src="#">Tienda</a>
+
+
+
+  </header>
+  );
+}
+
+export default Navbar;
